c02618a4eaa621da24e7f88e9a0f191bec0ee973
yungshun317 <seraph317@gmail.com>
2021-07-02 13:58:00 +0800

    [CHORE] Add the `spring-boot-starter-data-jpa` dependency to `pom.xml` for using `@Id` and `@Entity`

--- src/main/java/yungshun/chang/springbootfooddeliverysecurity/Ingredient.java
+++ src/main/java/yungshun/chang/springbootfooddeliverysecurity/Ingredient.java
@@ -5,12 +5,15 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
 @Data
 @RequiredArgsConstructor
-@NoArgsConstructor(access= AccessLevel.PRIVATE, force=true)
+@NoArgsConstructor(access= AccessLevel.PUBLIC, force=true)
 @Entity
 public class Ingredient {
-    
+
     @Id
     private final String id;
     private final String name;
