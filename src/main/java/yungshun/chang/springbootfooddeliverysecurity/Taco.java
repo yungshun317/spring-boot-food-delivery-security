3bdbb8eae09855dfb5710dd4cda11c4dbf66beae
yungshun317 <seraph317@gmail.com>
2021-07-03 12:39:27 +0800

    [FEAT] Use the `@Id`, `@GeneratedValue`, `@NotNull`, `@ManyToMany`, and `@PrePersist` annotations in `Taco`

--- src/main/java/yungshun/chang/springbootfooddeliverysecurity/Taco.java
+++ src/main/java/yungshun/chang/springbootfooddeliverysecurity/Taco.java
@@ -1,19 +1,29 @@
 package yungshun.chang.springbootfooddeliverysecurity;
 
+import com.sun.istack.NotNull;
+
+import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
 public class Taco {
 
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
     private Long id;
 
+    @NotNull
+    @Size(min=5, message="Name must be at least 5 characters long")
     private String name;
 
     private Date createdAt;
 
+    @ManyToMany(targetEntity = Ingredient.class)
+    @Size(min=1, message="You must choose at least 1 ingredient")
     private List<Ingredient> ingredients = new ArrayList<>();
 
+    @PrePersist
     void createdAt() {
         this.createdAt = new Date();
     }
