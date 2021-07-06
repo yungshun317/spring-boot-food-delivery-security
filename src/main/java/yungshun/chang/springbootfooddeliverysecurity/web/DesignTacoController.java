054f5a3190da5380749a33f88dc05f0d3f13923d
yungshun317 <seraph317@gmail.com>
2021-07-06 14:02:39 +0800

    [FEAT] Bind method `return` values to models with `@ModelAttribute`

--- src/main/java/yungshun/chang/springbootfooddeliverysecurity/web/DesignTacoController.java
+++ src/main/java/yungshun/chang/springbootfooddeliverysecurity/web/DesignTacoController.java
@@ -1,6 +1,9 @@
 package yungshun.chang.springbootfooddeliverysecurity.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import yungshun.chang.springbootfooddeliverysecurity.Order;
+import yungshun.chang.springbootfooddeliverysecurity.Taco;
 import yungshun.chang.springbootfooddeliverysecurity.data.IngredientRepository;
 import yungshun.chang.springbootfooddeliverysecurity.data.TacoRepository;
 
@@ -13,5 +16,15 @@ public class DesignTacoController {
     public DesignTacoController(IngredientRepository ingredientRepo, TacoRepository tacoRepo) {
         this.ingredientRepo = ingredientRepo;
         this.tacoRepo = tacoRepo;
+    }
+
+    @ModelAttribute(name="order")
+    public Order order() {
+        return new Order();
+    }
+
+    @ModelAttribute(name="design")
+    public Taco design() {
+        return new Taco();
     }
 }
